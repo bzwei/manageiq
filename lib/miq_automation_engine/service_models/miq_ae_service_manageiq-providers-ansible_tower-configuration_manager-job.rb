45a615f55fc0efa2cdb020335088063f772ad0f7
Bill Wei <bilwei@redhat.com>
2016-03-18 13:30:17 -0400

    Introduce AnsibleTower's Job subclassed from OrchestrationStack

---
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-ansible_tower-configuration_manager-job.rb
@@ -0,0 +1,5 @@
+module MiqAeMethodService
+  class MiqAeServiceManageIQ_Providers_AnsibleTower_ConfigurationManager_Job < MiqAeServiceOrchestrationStack
+    expose :job_template, :association => true
+  end
+end
